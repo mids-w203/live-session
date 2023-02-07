fb4499a1cd6170fc871de0e37d47f8d483b36f41
Alex Hughes <dhughes@berkeley.edu>
2023-02-07 14:47:42 -0800

    switch render order

--- src/render_book.sh
+++ src/render_book.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-Rscript -e "bookdown::render_book(input = '.', clean = TRUE, output_format = 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book(input = '.', clean = TRUE, output_format = 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book(input = '.', clean = TRUE, output_format = 'bookdown::pdf_book')"
